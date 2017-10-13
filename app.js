3a494d44018f744c409dbf9f440cd84eb14e1ee9
peeyushjss <peeyushjss@gmail.com>
2017-10-13 23:43:17 +0530

    Task complelted and tested successfully.

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const index = require('./routes/index');
+const session = require('express-session');
+
+const app = express();
+
+app.listen(7000);
+
+app.set('views', path.join(__dirname, 'view'));
+app.engine('html', require('ejs').renderFile);
+//app.use(express.static(__dirname + '/public'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: 'ssshhhhh'}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next();
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({status: 500, message: 'error'});
+});
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,201 +1,72 @@
-var express = require('express');
-var router = express.Router();
-var crypto = require('crypto');
-var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'sampleapp1891@gmail.com',
-        pass: 'Papaisgreat1@'
-    }
-});
-router.get('/', function (req, res) {
-    res.redirect('/login');
-});
-router.get('/login', function (req, res, next) {
-    res.render('login.html', {
-        title: "Login"
-    });
-});
-router.get('/register', function (req, res, next) {
-    res.render('register.html', {
-        title: "Register"
-    });
-});
-router.get('/reset', function (req, res, next) {
-    res.render('reset.html', {
-        title: "reset"
-    });
-});
-/* POST API for Register User. */
-router.post('/register', function (req, res, next) {
-    var usersDB = req.users;
-    var name = req.body.name;
-    var email = req.body.email;
-    var password = req.body.password;
-    var cpassword = req.body.cpassword;
-    var incryptPassword = '';
-    var validEmail = false;
-    var algorithm = 'aes-256-ctr',
-            predefinedPassword = 'd6F3Efeq';
-    if (password !== cpassword) {
-        req.json_op_status = 0;
-        req.json_op_message = 'Password does not match';
-        next();
-    }
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const session = require('express-session');
 
-    if (password) {
-        var cipher = crypto.createCipher(algorithm, predefinedPassword);
-        var incryptPassword = cipher.update(password, 'utf8', 'hex');
-        incryptPassword += cipher.final('hex');
+router.get('/', function(req, res) {
+    res.redirect('/settings');
+});
+
+router.get('/settings', function(req, res, next) {
+    res.render('settings.html', {
+        title: "Settings"
+    });
+});
+
+router.post('/settings', function(req, res, next) {
+    if (req.body.email && req.body.password) {
+        req.session.email = req.body.email;
+        req.session.password = req.body.password;
+        res.redirect('/email');
     } else {
-        req.json_op_status = 0;
-        req.json_op_message = 'Password required';
-        next();
+        res.render('error.html', {
+            title: "Error"
+        });
     }
+})
 
-    function validateEmail(email) {
-        var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return regex.test(email);
-    }
+router.get('/email', function(req, res, next) {
+    res.render('email.html', {
+        title: "Email"
+    });
+});
 
-    if (email) {
-        validEmail = validateEmail(email);
-    }
+router.post('/email', function(req, res, next) {
+    let bodyData = req.body;
+    let sessionData = req.session;
+    if (sessionData.email && sessionData.password && bodyData.to && bodyData.body && bodyData.subject) {
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: sessionData.email,
+                pass: sessionData.password
+            }
+        });
 
-    if (!validEmail) {
-        req.json_op_status = 0;
-        req.json_op_message = 'Invaild Email';
-        next();
-    }
-
-    if (name && email && password) {
-        usersDB.findOne({
-            email: email
-        }, function (error, records) {
+        let mailOptions = {
+            from: sessionData.email,
+            to: bodyData.to,
+            subject: bodyData.subject,
+            text: 'Your Password is : Peeyush.',
+            html: '<h3>' + bodyData.body + '</h3>'
+        };
+        transporter.sendMail(mailOptions, function(error, info) {
             if (error) {
-                req.json_op_status = 0;
-                req.json_op_message = error;
-                next();
-            } else if (!records) {
-                var record = new usersDB({
-                    name: name,
-                    email: email,
-                    password: incryptPassword
-                });
-                record.save(function (err) {
-                    if (err) {
-                        req.json_op_status = 0;
-                        req.json_op_message = err;
-                        next();
-                    } else {
-                        res.render('welcome.html', {
-                            title: "Welcome"
-                        });
-                    }
+                res.render('error.html', {
+                    title: "Error"
                 });
             } else {
-                req.json_op_status = 0;
-                req.json_op_message = 'User already registered';
-                next();
+                sessionData.destroy();
+                res.render('success.html', {
+                    title: "Success"
+                });
             }
         });
     } else {
-        req.json_op_status = 0;
-        req.json_op_message = 'Fill all fields.';
-        next();
+        res.render('error.html', {
+            title: "Error"
+        });
     }
 });
 
-/* POST API for Login User. */
-router.post('/login', function (req, res, next) {
-    var usersDB = req.users;
-    var email = req.body.email;
-    var password = req.body.password;
-    var incryptPassword = '';
-    var algorithm = 'aes-256-ctr',
-            predefinedPassword = 'd6F3Efeq';
-    if (password) {
-        var cipher = crypto.createCipher(algorithm, predefinedPassword);
-        var incryptPassword = cipher.update(password, 'utf8', 'hex');
-        incryptPassword += cipher.final('hex');
-    } else {
-        req.json_op_status = 0;
-        req.json_op_message = 'Password can not be blank';
-        next();
-    }
-    if (email && password) {
-        usersDB.findOne({
-            email: email,
-            password: incryptPassword
-        }, function (error, records) {
-            if (error) {
-                req.json_op_status = 0;
-                req.json_op_message = error;
-                next();
-            } else if (records) {
-                res.render('welcome.html', {
-                    title: "Welcome"
-                });
-            } else {
-                req.json_op_status = 0;
-                req.json_op_message = 'Invalid Credentails.';
-                next();
-            }
-        });
-    } else {
-        req.json_op_status = 0;
-        req.json_op_message = 'Fill all fields';
-        next();
-    }
-});
-
-/* POST API for Reset Password for User. */
-router.post('/reset', function (req, res, next) {
-    var algorithm = 'aes-256-ctr',
-            predefinedPassword = 'd6F3Efeq';
-    var usersDB = req.users;
-    var email = req.body.email;
-    if (email) {
-        usersDB.findOne({
-            email: email
-        }, function (err, result) {
-            if (err) {
-                req.json_op_status = 0;
-                req.json_op_message = err;
-                next();
-            } else if (result) {
-                var decPassword = result.password;
-                var decipher = crypto.createDecipher(algorithm, predefinedPassword);
-                var dec = decipher.update(decPassword, 'hex', 'utf8');
-                dec += decipher.final('utf8');
-                let mailOptions = {
-                    from: 'sampleapp1891@gmail.com',
-                    to: email,
-                    subject: 'New Password',
-                    text: 'Your Password is : ' + dec + '.',
-                    html: '<h3>Your Password is : ' + dec + '.</h3>'
-                };
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        req.json_op_status = 0;
-                        req.json_op_message = error;
-                        next();
-                    }
-                    req.json_op_status = 1;
-                    req.json_op_message = 'Check your mail account.';
-                    next();
-                });
-            } else {
-                req.json_op_status = 0;
-                req.json_op_message = 'User does not exist';
-                next();
-            }
-        });
-    } else {
-        req.json_op_status = 0;
-        req.json_op_message = 'Email can not be blank';
-        next();
-    }
-});
 module.exports = router;
